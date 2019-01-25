26be00d07ad24ad2ea1964df5f3b708c62137f6a
Zunayeed <zunayeed.edu@gmail.com>
2019-01-25 13:21:05 -0500

    Create Test_class.java

---
+++ src/package_1/Test_class.java
@@ -0,0 +1,18 @@
+package package_1;
+
+public class Test_class {
+
+	public static void main(String[] args) {
+			System.out.println("hello zunayeed try   first package ?");
+			String tree = "pine"; 
+			int count = 0 ; 
+			if(tree.equals("pine")) {
+			  int  height = 55; 
+				count = count + 1 ;
+				System.out.println(height*count);
+			}
+			
+			//System.out.println(height*count);
+
+       } 
+} 
