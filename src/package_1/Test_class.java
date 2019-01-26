92396939742dd2ef96d43737251f649113f18110
Zunayeed <zunayeed.edu@gmail.com>
2019-01-26 11:49:36 -0500

    Update Test_class.java

--- src/package_1/Test_class.java
+++ src/package_1/Test_class.java
@@ -4,7 +4,7 @@ public class Test_class {
 
 	public static void main(String[] args) {
 			System.out.println("hello zunayeed try this one   first package ?");
-			System.out.println("we had to be careful when we check ");
+			System.out.println("we had to be careful when we check hello ");
 			String tree = "pine"; 
 			int count = 0 ; 
 			if(tree.equals("pine")) {
