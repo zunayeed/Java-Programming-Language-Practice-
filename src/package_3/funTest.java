5e5fdd3e32fd5e444c13dac29280af6b6b1aaac0
Zunayeed <zunayeed.edu@gmail.com>
2019-01-27 09:23:30 -0500

    Create funTest.java

---
+++ src/package_3/funTest.java
@@ -0,0 +1,8 @@
+package package_3;
+
+public class funTest {
+public static void main(String... deerParams) {
+	System.out.println();
+	System.out.println(" main method, just press ma then control + space ");
+}
+ }
