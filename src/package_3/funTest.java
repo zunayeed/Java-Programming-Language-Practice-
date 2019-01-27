3a3d8a1d7aa222b64a7303b2270f844cc9f0b14e
Zunayeed <zunayeed.edu@gmail.com>
2019-01-27 10:53:37 -0500

    Update funTest.java

--- src/package_3/funTest.java
+++ src/package_3/funTest.java
@@ -4,5 +4,6 @@ public class funTest {
 public static void main(String... deerParams) {
 	System.out.println();
 	System.out.println(" main method, just press ma then control + space ");
+	System.out.println(theInput[2]);
 }
  }
