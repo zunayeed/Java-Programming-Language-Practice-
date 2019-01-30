09289d3070de20e3669ff2a22c6a2296d9e3181b
Zunayeed <zunayeed.edu@gmail.com>
2019-01-29 19:29:57 -0500

    Create JavaSETest.java

---
+++ src/package_3/JavaSETest.java
@@ -0,0 +1,14 @@
+package package_3;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class JavaSETest {
+ public static void main(String[] args) {
+ List<Integer> elements = new ArrayList<>();
+ elements.add(10);
+ int firstElmnt = elements.get(1);
+ System.out.println(firstElmnt);
+ }
+}
+
