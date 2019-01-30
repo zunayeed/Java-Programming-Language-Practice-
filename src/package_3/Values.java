f294ebb6bd08d0ca3622c9c31742948c89091afd
Zunayeed <zunayeed.edu@gmail.com>
2019-01-30 00:09:00 -0500

    Create Values.java

---
+++ src/package_3/Values.java
@@ -0,0 +1,10 @@
+package package_3;
+
+public class Values {
+	Integer a = Integer.valueOf("1");
+  public static void main(String[] nums) {
+	  	Integer a = Integer.valueOf("2");
+	  	Integer b = Integer.valueOf("3");
+	  		System.out.println(a + b);
+  		}
+}
