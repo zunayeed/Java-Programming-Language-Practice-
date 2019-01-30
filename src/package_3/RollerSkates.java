6186770dde0bda8f45a22ea360693557411ee10e
Zunayeed <zunayeed.edu@gmail.com>
2019-01-29 19:30:23 -0500

    Create RollerSkates.java

---
+++ src/package_3/RollerSkates.java
@@ -0,0 +1,13 @@
+package package_3;
+
+public class RollerSkates {
+	static int wheels = 1 ; 
+	int tracks = 5; 
+	public static void main(String[] args) {
+		RollerSkates s = new RollerSkates();
+		int feet = 4, tracks = 15; 
+		System.out.println(feet + tracks + s.wheels);
+
+	}
+
+}
