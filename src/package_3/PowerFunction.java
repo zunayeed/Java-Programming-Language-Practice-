345b1f1a1c0309fe93a7e725587609a6f309795b
Zunayeed <zunayeed.edu@gmail.com>
2019-01-30 20:21:40 -0500

    Update PowerFunction.java

--- src/package_3/PowerFunction.java
+++ src/package_3/PowerFunction.java
@@ -1,8 +1,15 @@
 package package_3;
-
+/* this program implements the power function  iteration from 1 */
 public class PowerFunction {
 
 	public static void main(String[] args) {
+		int base = 5 ; 
+		int exponent = 2;
+		int result = base ;
+		for(int i = 1; i< exponent; i++) {
+			result = result * base ; 
+		}
+		System.out.println(result);
 		
 	}
 
