5f810a05e2e5b664471e87cd0d415619e8c27239
Zunayeed <zunayeed.edu@gmail.com>
2019-02-01 10:30:35 -0500

    Create ModulusFunction.java

---
+++ src/package_3/ModulusFunction.java
@@ -0,0 +1,12 @@
+package package_3;
+
+public class ModulusFunction {
+
+	public static void main(String[] args) {
+		int num = 100 ;
+			int 	divisor	= 7;
+			int rem = num - (num/divisor)*divisor ; 
+		System.out.println(rem);	
+	}
+
+}
