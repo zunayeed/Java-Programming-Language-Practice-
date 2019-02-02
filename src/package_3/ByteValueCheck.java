5324015946542484b49a9b235755edc70d7c54e5
Zunayeed <zunayeed.edu@gmail.com>
2019-02-02 14:13:43 -0500

    Create ByteValueCheck.java

---
+++ src/package_3/ByteValueCheck.java
@@ -0,0 +1,13 @@
+package package_3;
+
+public class ByteValueCheck {
+
+	public static void main(String[] args) {
+		Integer integer = new Integer(67); 
+		System.out.println(integer.byteValue());
+		
+		int i =  new Integer(4); 
+		System.out.println(i.byteValue());
+	}
+
+}
