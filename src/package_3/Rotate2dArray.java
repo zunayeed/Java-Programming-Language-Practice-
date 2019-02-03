cb78733f57d3259b4ad263a5b9bc88f9c94898f3
Zunayeed <zunayeed.edu@gmail.com>
2019-02-03 17:47:47 -0500

    Update Rotate2dArray.java

--- src/package_3/Rotate2dArray.java
+++ src/package_3/Rotate2dArray.java
@@ -3,8 +3,13 @@ package package_3;
 public class Rotate2dArray {
 
 	public static void main(String[] args) {
-		int[][] muiltiDimArray = new int[3][3];
-		System.out.println(multiDimArray);
+		int[][] mArray = new int[4][2];
+		  for(int i = 0; i <mArray.length; i++) {
+			  for( int j =0; j < mArray[i].length; j++)
+				  System.out.println(mArray[i][j]+ " ");
+			  
+		  }
+		
 
 	}
 
