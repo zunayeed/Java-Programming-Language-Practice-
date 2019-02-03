60a6d9bc78c98430d0ce2a17911add902a9d139b
Zunayeed <zunayeed.edu@gmail.com>
2019-02-03 15:53:26 -0500

    Update Rotate2dArray.java

--- src/package_3/Rotate2dArray.java
+++ src/package_3/Rotate2dArray.java
@@ -1,9 +1,10 @@
 package package_3;
-
+/* rotate a 2D matrix by 90 degree */
 public class Rotate2dArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] muiltiDimArray = new int[3][3];
+		System.out.println(sizeOff(multiDimArray.toString()));
 
 	}
 
