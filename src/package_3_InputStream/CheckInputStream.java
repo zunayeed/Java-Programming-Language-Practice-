080f354efc7c319672a8bbd2b0b63cd380cb2a45
Zunayeed <zunayeed.edu@gmail.com>
2019-02-03 10:55:35 -0500

    Update CheckInputStream.java

--- src/package_3_InputStream/CheckInputStream.java
+++ src/package_3_InputStream/CheckInputStream.java
@@ -1,10 +1,39 @@
 package package_3_InputStream;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+
 public class CheckInputStream {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+	      InputStream is = null;
+	      int i;
+	      char c;
+	      
+	      try {
+	         // new input stream created
+	         is = new FileInputStream("C://zunayeed.txt");
+	         
+	         System.out.println("Characters printed:");
+	         
+	         // reads till the end of the stream
+	         while((i = is.read())!=-1) {
+	         
+	            // converts integer to character
+	            c = (char)i;
+	            
+	            // prints character
+	            System.out.print(c);
+	         }
+	         
+	      } catch(Exception e) {
+	         // if any I/O error occurs
+	         e.printStackTrace();
+	      } finally {
+	         // releases system resources associated with this stream
+	         if(is!=null)
+	            is.close();
+	      }
+	   }
 	}
-
-}
