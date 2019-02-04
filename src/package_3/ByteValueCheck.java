52d916e9cfbbd05e4f32f7b7dc8cd652b7989501
Zunayeed <zunayeed.edu@gmail.com>
2019-02-04 02:36:40 -0500

    Update ByteValueCheck.java

--- src/package_3/ByteValueCheck.java
+++ src/package_3/ByteValueCheck.java
@@ -7,8 +7,8 @@ public class ByteValueCheck {
 		System.out.println(integer.byteValue());
 		
 /* java does to allow calling method on primitive so this code will not compile*/
-		int i =  new Integer(4); 
-		System.out.println(i.byteValue());
+		// int i =  new Integer(4); 
+	//	System.out.println(i.byteValue());
 	}
 
 }
