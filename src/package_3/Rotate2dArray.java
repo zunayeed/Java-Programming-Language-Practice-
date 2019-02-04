3b6d7cafb81df588b4fa8422926da340200cbac4
Zunayeed <zunayeed.edu@gmail.com>
2019-02-04 02:23:22 -0500

    Update Rotate2dArray.java

--- src/package_3/Rotate2dArray.java
+++ src/package_3/Rotate2dArray.java
@@ -17,6 +17,14 @@ public class Rotate2dArray {
 		for (int print: numbers) {
 		   System.out.println(print); 
 		}
+		
+		int[][] array2d = {{1, 2, 3}, {4, 5, 6}};
+		for (int[] array : array2d) {
+		    for (int element : array) {
+		        System.out.print(element + " ");
+		    }
+		    System.out.println();
+		}
 	}
 
 }
