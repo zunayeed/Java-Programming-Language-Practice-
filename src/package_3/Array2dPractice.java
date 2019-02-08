2f9db8fe0010b167f84d849adf8526d39efeb682
Zunayeed <zunayeed.edu@gmail.com>
2019-02-08 13:50:28 -0500

    Create Array2dPractice.java

---
+++ src/package_3/Array2dPractice.java
@@ -0,0 +1,15 @@
+package package_3;
+
+import java.util.Scanner;
+
+public class Array2dPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("print some number");
+		Scanner scan = new Scanner(System.in);
+		scan.next(); // returns the next token of input
+		
+	}
+
+}
