3fdad9a856dd6ab7091ba9396550ce704fe0392b
Zunayeed <zunayeed.edu@gmail.com>
2019-02-08 13:50:36 -0500

    Create PrintArrayPractice.java

---
+++ src/package_3/PrintArrayPractice.java
@@ -0,0 +1,15 @@
+package package_3;
+
+import java.util.Arrays;
+
+public class PrintArrayPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    int[ ] numbers = {2, 3, 5, 7};    
+			String[] nums = new String[] {"1","9","10"};
+			System.out.println(Arrays.toString(nums));
+			System.out.println(Arrays.toString(numbers));
+	}
+
+}
