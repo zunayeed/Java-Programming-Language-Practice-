3ead5d2bc376dab7e3023042b2f52196edab47cc
Zunayeed <zunayeed.edu@gmail.com>
2019-02-22 13:30:16 -0500

    Create Remember.java

---
+++ src/package_3/Remember.java
@@ -0,0 +1,21 @@
+package package_3;
+
+public class Remember {
+		
+
+		public static void think() throws Exception { // k1
+		try {
+		throw new Exception();
+			}finally {
+				System.out.println("hello zunayeed ");
+			}
+		}
+		
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+	   think();
+
+	}
+
+}
