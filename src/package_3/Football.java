ca01b8f4e32f9dc0c77210fd989bf29828e9de93
Zunayeed <zunayeed.edu@gmail.com>
2019-02-22 18:23:48 -0500

    Update Football.java

--- src/package_3/Football.java
+++ src/package_3/Football.java
@@ -3,14 +3,14 @@ package package_3;
 public class Football {
 
 	public static void main(String[] args) {
-		private  static Long getScore(Long timeRemaining) {
-			return 2*timeRemaining;
+	//	private  static Long getScore(Long timeRemaining) {
+		//	return 2*timeRemaining;
 		}
-		 public static void main(String[] args) {
-			 final int startTime = 4 ; 
-			 System.out.println(getScore(startTime));
-		 }
+	//	 public static void main(String[] args) {
+		//	 final int startTime = 4 ; 
+		//	 System.out.println(getScore(startTime));
+	//	 }
 
 	}
 
-}
+//}
