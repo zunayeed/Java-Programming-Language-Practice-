ee65cd9e6ea4e095c0b4cda1e035ea6b5461ca48
Zunayeed <zunayeed.edu@gmail.com>
2019-02-22 18:23:43 -0500

    Create Baseball.java

---
+++ src/package_3/Baseball.java
@@ -0,0 +1,22 @@
+package package_3;
+
+public class Baseball {
+
+	public static void main(String[] args) {
+		int score = 0 ; 
+		try {
+			 score = 1;
+			System.out.print(score++);
+			} catch (Throwable t) {
+			System.out.print(score++);
+			} finally {
+			System.out.print(score++);
+			}
+			System.out.print(score++);
+			}
+			
+		
+
+	}
+
+
