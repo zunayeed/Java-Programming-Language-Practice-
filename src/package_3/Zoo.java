786eec2472c2ce5e5978623e84ab7b38ee90df39
Zunayeed <zunayeed.edu@gmail.com>
2019-02-22 18:23:38 -0500

    Update Zoo.java

--- src/package_3/Zoo.java
+++ src/package_3/Zoo.java
@@ -3,9 +3,12 @@ package package_3;
 public class Zoo {
 
 	public static void main(String[] args) {
-		System.out.println(args[0]);
+		// System.out.println(args[0]);
 		// System.out.println(args[1]);
-
+        int i = 1; 
+        int  j = ++i ; 
+        int k = i++ ; 
+        System.out.println("i   " + i + "   j  "+ j + "  K "+   k);
 	}
 
 }
