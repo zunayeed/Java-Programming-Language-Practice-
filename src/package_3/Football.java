b6e1df30ab00209eede16b86f8e81aefbb23f019
Zunayeed <zunayeed.edu@gmail.com>
2019-02-22 12:38:05 -0500

    Create Football.java

---
+++ src/package_3/Football.java
@@ -0,0 +1,16 @@
+package package_3;
+
+public class Football {
+
+	public static void main(String[] args) {
+		private  static Long getScore(Long timeRemaining) {
+			return 2*timeRemaining;
+		}
+		 public static void main(String[] args) {
+			 final int startTime = 4 ; 
+			 System.out.println(getScore(startTime));
+		 }
+
+	}
+
+}
