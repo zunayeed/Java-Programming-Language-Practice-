4ca95094585516facaf3250d0a5d9ce009372e66
Zunayeed <zunayeed.edu@gmail.com>
2019-02-24 09:49:06 -0500

    Create Boat.java

---
+++ src/package_3/Boat.java
@@ -0,0 +1,13 @@
+package package_3;
+
+public class Boat {
+
+	public int travel() throws Exception { return 4; }; // j1
+	public static void main(String... distance) throws Exception{
+	try {
+	System.out.print(new Boat().travel());
+	} catch (Exception e) {
+		System.out.print(8);
+		}
+	}
+	}
