84720f28343e46fbb54f054ce7c3d97a296f054e
Zunayeed <zunayeed.edu@gmail.com>
2019-02-24 01:04:39 -0500

    Update BasketBall.java

--- src/package_3/BasketBall.java
+++ src/package_3/BasketBall.java
@@ -3,7 +3,17 @@ package package_3;
 public class BasketBall {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			System.out.print(1);
+			throw new ClassCastException();
+			} catch (ArrayIndexOutOfBoundsException ex) {
+			System.out.print(2);
+			} catch (Throwable ex) {
+			System.out.print(3);
+			} finally {
+			System.out.print(4);
+			}
+			System.out.print(5);
 
 	}
 
