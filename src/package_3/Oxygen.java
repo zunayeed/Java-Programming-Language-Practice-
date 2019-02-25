e9d84c43daa73ec61cdf868c4128698fa7796783
Zunayeed <zunayeed.edu@gmail.com>
2019-02-25 06:07:14 -0500

    Update Oxygen.java

--- src/package_3/Oxygen.java
+++ src/package_3/Oxygen.java
@@ -1,5 +1,13 @@
 package package_3;
 
-public class Oxygen {
-
+public class Oxygen extends Element {
+	public int getSymbol() { return 8; } // g2
+	public void printData() {
+	try {
+	System.out.print(getSymbol());
+	} catch(Exception e ) 
+	{ // g3
+	System.out.print("Unable to read data");
+	}
+	}
 }
