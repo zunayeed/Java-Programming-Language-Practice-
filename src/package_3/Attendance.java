9208c9008a35ffa62587954557198f09fa595876
Zunayeed <zunayeed.edu@gmail.com>
2019-02-27 17:31:27 -0500

    Create Attendance.java

---
+++ src/package_3/Attendance.java
@@ -0,0 +1,17 @@
+package package_3;
+
+public class Attendance {
+	private Boolean[] list = // value omitted
+			public int printTodaysCount() {
+			int count=0;
+			for(int i=0; i<10; i++) {
+			if(list[i]) ++count;
+			}
+			return count;
+			}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Attendance().printTodaysCount();
+	}
+
+}
