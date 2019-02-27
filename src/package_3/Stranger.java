447dd7d36fc2642a74f4dfeba71a7aed16a8cf11
Zunayeed <zunayeed.edu@gmail.com>
2019-02-27 17:51:45 -0500

    Create Stranger.java

---
+++ src/package_3/Stranger.java
@@ -0,0 +1,18 @@
+package package_3;
+
+public class Stranger {
+	public static String getFullName(String firstName, String lastName) {
+		try {
+		return firstName.toString() + " " + lastName.toString();
+		}catch (NullPointerException npe) {
+		System.out.print("Problem?");
+		} finally {
+			System.out.print("Finished!");
+			} 
+		return null;
+		}
+		public static void main(String[] things) {
+		System.out.print(getFullName("Joyce","Hopper"));
+		}
+		
+}
