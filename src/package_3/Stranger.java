5fbba39496bce37f312c0306ec42b4dabb81f90a
Zunayeed <zunayeed.edu@gmail.com>
2019-02-28 21:37:39 -0500

    Update Stranger.java

--- src/package_3/Stranger.java
+++ src/package_3/Stranger.java
@@ -3,12 +3,14 @@ package package_3;
 public class Stranger {
 	public static String getFullName(String firstName, String lastName) {
 		try {
+			System.out.println("before return");
 		return firstName.toString() + " " + lastName.toString();
 		}catch (NullPointerException npe) {
 		System.out.print("Problem?");
 		} finally {
 			System.out.print("Finished!");
 			} 
+		
 		return null;
 		}
 		public static void main(String[] things) {
