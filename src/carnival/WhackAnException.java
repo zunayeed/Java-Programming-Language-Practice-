bbb39fbaceb9581343aceba4e80f6703864eafaf
Zunayeed <zunayeed.edu@gmail.com>
2019-03-02 11:21:12 -0500

    Update WhackAnException.java

--- src/carnival/WhackAnException.java
+++ src/carnival/WhackAnException.java
@@ -1,5 +1,15 @@
 package carnival;
 
 public class WhackAnException {
-
+	public static void main(String... hammer) {
+		try {
+		throw new ClassCastException();
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException();
+			} catch (RuntimeException e) {
+			throw new NullPointerException();
+			} finally {
+		//	throw new RuntimeException();
+			}
+			}
 }
