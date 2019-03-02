818ccd96d11dbb52162a71fcd682ecbc55453cb8
Zunayeed <zunayeed.edu@gmail.com>
2019-03-02 07:48:27 -0500

    Create ZooWorker.java

---
+++ src/polymorphic_parameter_test/ZooWorker.java
@@ -0,0 +1,13 @@
+package polymorphic_parameter_test;
+
+public class ZooWorker {
+	public static void feed(Reptile reptile) {
+		System.out.println("Feeding reptile "+reptile.getName());
+		}
+	public static void main(String[] args) {
+		feed(new Alligator());
+		feed(new Crocodile());
+		feed(new Reptile());
+		}
+
+}
