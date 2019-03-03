4fb2714d31f4e769dc0919927296fc30d410a23e
Zunayeed <zunayeed.edu@gmail.com>
2019-03-03 18:25:17 -0500

    Create Robot.java

---
+++ src/ai/Robot.java
@@ -0,0 +1,12 @@
+package ai;
+
+public class Robot {
+	  public int compute() { return 104; }
+	public static void main(String[] args) {
+		System.out.println(new Robot().compute());
+	}
+    
+		
+
+		}
+
