1556861aea26d6c81909a01cd3b9a24cedd12dd6
Zunayeed <zunayeed.edu@gmail.com>
2019-03-03 20:55:08 -0500

    Create Jump.java

---
+++ src/games/Jump.java
@@ -0,0 +1,16 @@
+package games;
+
+public class Jump {
+	private int rope = 1;
+	protected boolean outside;
+	public Jump() {
+	      this(4) ;   // p1
+	outside = true;
+	}
+	public Jump(int rope) {
+	this.rope = outside ? rope : rope+1;
+	}
+	public static void main(String[] bounce) {
+	System.out.print(new Jump().rope);
+	}
+}
