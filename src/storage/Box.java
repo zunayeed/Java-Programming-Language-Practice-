7b549df77e75544778d2da31342da41f8f0c3b96
Zunayeed <zunayeed.edu@gmail.com>
2019-03-03 20:54:55 -0500

    Create Box.java

---
+++ src/storage/Box.java
@@ -0,0 +1,12 @@
+package storage;
+
+public class Box {
+
+	public String stuff;
+	public String  getStuff() {
+	return stuff;
+	}
+	public void setStuff(String stuff) {
+	this.stuff = stuff;
+	}
+}
