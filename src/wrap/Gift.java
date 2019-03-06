7d5e1f692138912a1d5f5dc0067d9843da59efd5
Zunayeed <zunayeed.edu@gmail.com>
2019-03-06 03:20:57 -0500

    Create Gift.java

---
+++ src/wrap/Gift.java
@@ -0,0 +1,19 @@
+package wrap;
+
+public class Gift {
+private  Object contents;
+protected Object getContents() {
+return contents;
+}
+protected void setContents(Object contents) {
+this.contents = contents;
+}
+public void showPresent() {
+System.out.print("Your gift: "+contents);
+}
+public static void main(String[] treats) {
+Gift gift = new Gift();
+gift.setContents(gift);
+gift.showPresent();
+}
+}
