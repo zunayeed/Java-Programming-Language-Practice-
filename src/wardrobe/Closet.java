a2ffecdd06af1c87d988df3848123fc2757c5eb5
Zunayeed <zunayeed.edu@gmail.com>
2019-03-06 04:15:00 -0500

    Update Closet.java

--- src/wardrobe/Closet.java
+++ src/wardrobe/Closet.java
@@ -1,5 +1,10 @@
 package wardrobe;
-
+//INSERT CODE HERE  
+ import static clothes.Store.getClothes;
+ //import clothes.Store.*;
+// import static clothes.Store;
 public class Closet {
-
+	public void borrow() {
+		System.out.print("Borrowing clothes: "+getClothes());
+		}
 }
