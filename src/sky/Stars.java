a8c476668a7ba94b2096da8b49de4733585f4d16
Zunayeed <zunayeed.edu@gmail.com>
2019-03-06 05:11:39 -0500

    Update Stars.java

--- src/sky/Stars.java
+++ src/sky/Stars.java
@@ -1,5 +1,15 @@
 package sky;
 
 public class Stars {
-
+	private int inThe = 4;
+	public Stars() {
+	super();
+	}
+	
+	public Stars(int inThe) {
+		this.inThe = inThe;
+		}
+		public static void main(String[] endless) {
+		System.out.print(new sky.Stars(2).inThe);
+		}
 }
