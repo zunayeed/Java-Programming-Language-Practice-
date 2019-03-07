91b1263bb8c7d14abdd955ac1c4a9e0f01a8ebd9
Zunayeed <zunayeed.edu@gmail.com>
2019-03-07 01:36:41 -0500

    Update Stars.java

--- src/sky/Stars.java
+++ src/sky/Stars.java
@@ -2,12 +2,12 @@ package sky;
 
 public class Stars {
 	private int inThe = 4;
-	public  void Stars() {
+	public   Stars() {
 	super();
 	}
 	
 	public Stars(int inThe) {
-		this.inThe = this.inThe;
+		this.inThe = inThe;
 		}
 		public static void main(String[] endless) {
 		System.out.print(new sky.Stars(2).inThe);
