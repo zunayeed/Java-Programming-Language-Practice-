5c455041519e103a8112ce80064ee5db91d08d53
Zunayeed <zunayeed.edu@gmail.com>
2019-03-08 17:22:13 -0500

    Create Tree.java

---
+++ src/ship/Tree.java
@@ -0,0 +1,16 @@
+package ship;
+
+public class Tree {
+		
+	public final static long numberOfTrees;
+	public final double height;
+	static {}
+	{ final int initHeight = 2;
+	height = initHeight;
+	}
+	static {
+	numberOfTrees = 100;
+	// height = 4;
+	}
+
+}
