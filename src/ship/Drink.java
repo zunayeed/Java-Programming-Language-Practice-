520be6b6b59560a660ec7b260d60e6b859cec1c5
Zunayeed <zunayeed.edu@gmail.com>
2019-03-08 11:24:45 -0500

    Create Drink.java

---
+++ src/ship/Drink.java
@@ -0,0 +1,13 @@
+package ship;
+
+public class Drink {
+	public static void water() {}
+	public void get() {
+	// INSERT CODE HERE
+		// this.Drink.water();
+		// this.water();
+		// Drink.water();
+		water();
+		
+	}
+}
