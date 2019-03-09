5b256ebed4f279fea735152c3ee17247ae77ad5f
Zunayeed <zunayeed.edu@gmail.com>
2019-03-08 21:39:29 -0500

    Update RainForest.java

--- src/jungle/RainForest.java
+++ src/jungle/RainForest.java
@@ -1,5 +1,13 @@
 package jungle;
 
 public class RainForest extends Forest {
-
+		
+	public RainForest(long treeCount) {
+		super( treeCount);
+		this.treeCount = treeCount+1;
+		}
+		public static void main(String[] birds) {
+		System.out.print(new RainForest(5).treeCount);
+		}
+	
 }
