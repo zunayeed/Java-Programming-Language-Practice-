73f4dbe16bead925a558547edc9f64375dc4b6da
Zunayeed <zunayeed.edu@gmail.com>
2019-03-09 20:44:19 -0500

    Update ElectricCar.java

--- src/vehicles/ElectricCar.java
+++ src/vehicles/ElectricCar.java
@@ -2,10 +2,23 @@ package vehicles;
 
 public class ElectricCar extends Car {
 	
-	public final String drive() { return "Driving electric car"; }
+	public  String drive() { return "Driving electric car"; }
 	public static void main(String[] wheels) {
-	final Car car = new ElectricCar();
-	System.out.print(car.drive());
+	
+		
+		
+	 Car car = new ElectricCar();
+	 System.out.println("reference car and object electric car:  " + car.drive());
+	 
+	 Car car1 =  new Car();
+	 System.out.println("    reference car and object car     :  "+car1.drive());
+	 
+	 Car car2 = (Car) new Automobile();
+	 System.out.println(car2.drive());
+	 
+	 Automobile auto1 = new Automobile(); 
+	 System.out.println(auto1.drive());
+	 
 	}
 
 }
