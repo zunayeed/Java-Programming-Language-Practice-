edbe4c50bb5af201df407841aaaafa6e826a1e5d
Zunayeed <zunayeed.edu@gmail.com>
2019-03-10 20:31:35 -0400

    Update ElectricCar.java

--- src/vehicles/ElectricCar.java
+++ src/vehicles/ElectricCar.java
@@ -1,24 +1,23 @@
 package vehicles;
-
+// exploring polymorphism 
 public class ElectricCar extends Car {
 	
 	public  String drive() { return "Driving electric car"; }
 	public static void main(String[] wheels) {
 	
-		
-		
 	 Car car = new ElectricCar();
-	 System.out.println("reference car and object electric car:  " + car.drive());
+	 System.out.println( car.drive());
 	 
-	 Car car1 =  new Car();
-	 System.out.println("    reference car and object car     :  "+car1.drive());
-	 
-	 Car car2 = (Car) new Automobile();
-	 System.out.println(car2.drive());
-	 
-	 Automobile auto1 = new Automobile(); 
+	 Automobile auto1 =  new Car(); // parent reference and child object
 	 System.out.println(auto1.drive());
 	 
+	 Automobile auto2 =  new ElectricCar(); // grand-parent reference and grand-son object
+	 System.out.println(auto2.drive());
+	 
+	 ElectricCar electric = (ElectricCar) new Car(); // child reference and parent object will not work
+	 System.out.println(electric.drive());
+	 
+	 Automobile auto3 =  new Automobile();
+	 System.out.println(auto3.drive());	 	  
 	}
-
 }
