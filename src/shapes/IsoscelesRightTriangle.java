f0677fbc897739872ffea951cf33f0b76ca01f13
Zunayeed <zunayeed.edu@gmail.com>
2019-03-11 13:19:38 -0400

    Update IsoscelesRightTriangle.java

--- src/shapes/IsoscelesRightTriangle.java
+++ src/shapes/IsoscelesRightTriangle.java
@@ -1,5 +1,9 @@
 package shapes;
 
 public abstract class IsoscelesRightTriangle extends RightTriangle {
-
+	public String getDescription() { return "irt"; }
+	public static void main(String[] edges) {
+	final Triangle shape = new IsoscelesRightTriangle(); // g3
+	System.out.print(shape.getDescription());
+	}
 }
