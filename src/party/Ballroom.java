9eada8d2b595ce6c3ea3a74becc753e5a7ced5a6
Zunayeed <zunayeed.edu@gmail.com>
2019-03-13 20:24:24 -0400

    Create Ballroom.java

---
+++ src/party/Ballroom.java
@@ -0,0 +1,8 @@
+package party;
+
+abstract public class Ballroom extends House {
+protected abstract Object getSpace();
+public static void main(String[] squareFootage) {
+System.out.print("Let's start the party!");
+}
+}
