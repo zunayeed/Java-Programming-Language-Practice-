2e827c6e225bd718cf4d2afb76d6300e6cf27c4c
Zunayeed <zunayeed.edu@gmail.com>
2019-03-15 18:38:18 -0400

    Create Road.java

---
+++ src/city/Road.java
@@ -0,0 +1,16 @@
+package city;
+
+public class Road {
+	public static void main(String... in) {
+		int intersections = 100;
+		int streets = 200;
+		if (intersections < 150) {
+		System.out.print("1");
+		} else if (streets / intersections > 0) {
+		System.out.print("2");
+		} if (streets < 500)
+		System.out.print("3");
+		else
+		System.out.print("4");
+		}
+}
