2490fdb97d9aa5c4741af22415db1a9dacb7dee7
Zunayeed <zunayeed.edu@gmail.com>
2019-03-15 18:05:06 -0400

    Create Transportation.java

---
+++ src/mode/Transportation.java
@@ -0,0 +1,11 @@
+package mode;
+
+public class Transportation {
+	public static String travel(int distance) {
+		return distance<1000 ? "train" : 12;
+		}
+		public static void main(String[] answer) {
+		System.out.print(Transportation.travel(500));
+		}
+
+}
