06196fa3c8ea776daf386c9ebbb7b3a0f372a22e
Zunayeed <zunayeed.edu@gmail.com>
2019-03-15 18:05:12 -0400

    Create Pieces.java

---
+++ src/restaurant/Pieces.java
@@ -0,0 +1,21 @@
+package restaurant;
+
+public class Pieces {
+	public static void main(String[] info) {
+		int flair = 15;
+		if(flair >= 15 && flair < 37) {
+		System.out.print("Not enough");
+		} if(flair==37) {
+			System.out.print("Just right");
+			} else {
+			System.out.println("Too many");
+			}
+	//	int time = 10 * (2 + (1 + 2 / 5) ;
+	//	int day = 5;
+		// String dinner = time > 10 ? day ? "Takeout" : "Salad" : "Leftovers";
+	 //   System.out.println(time/day);	
+	    System.out.println(5 + 6 + "7" + 8 + 9);
+			}
+	
+}
+
