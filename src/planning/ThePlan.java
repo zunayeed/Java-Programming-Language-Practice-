493ec4076709b83d57d07826f3d60f17a5890d1e
Zunayeed <zunayeed.edu@gmail.com>
2019-03-15 08:45:17 -0400

    Update ThePlan.java

--- src/planning/ThePlan.java
+++ src/planning/ThePlan.java
@@ -5,7 +5,9 @@ public class ThePlan {
 		
 	int plan = 1;
 	//System.out.println(plan++);
+	// plan = plan++; 
 	plan = plan++ + --plan;
+	//plan =  --plan;
 	System.out.println(plan);// 2
 	if(plan==1) {
 	System.out.print("Plan A");
