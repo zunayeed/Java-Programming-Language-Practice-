3f181f6039feeeaab40249be1e24cb09323d8f35
Zunayeed <zunayeed.edu@gmail.com>
2019-03-14 23:14:55 -0400

    Create VariablesEx.java

---
+++ src/ai/VariablesEx.java
@@ -0,0 +1,19 @@
+package ai;
+
+public class VariablesEx {
+
+	public static void main(String[] args) {
+		int EmpId = 101; 
+		String Ename = "John"; 
+		char Gender = 'M';
+		float Salary = 7677.98f ; 
+		boolean isFullTime = true; 
+		System.out.println(EmpId);
+		System.out.println(Ename);
+		System.out.println(Gender);
+		System.out.println(Salary);
+		System.out.println(isFullTime);
+		
+	}
+
+}
