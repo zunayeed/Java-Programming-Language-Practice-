d62b4d012ac54f210af6ca6898ffa21b59e7232f
Zunayeed <zunayeed.edu@gmail.com>
2019-03-16 12:08:34 -0400

    Update Transportation.java

--- src/mode/Transportation.java
+++ src/mode/Transportation.java
@@ -7,5 +7,10 @@ public class Transportation {
 		public static void main(String[] answer) {
 		System.out.println(Transportation.travel(500));
 		System.out.println(answer.length);
+		int characters = 5;
+		int story = 3;
+		double movieRating = characters <= 4 ? 3 : story>1 ? 2 : 1;
+		System.out.println(movieRating);
+		
 		}
 }
