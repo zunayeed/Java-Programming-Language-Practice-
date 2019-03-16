62271c0f6f399f38a18b7e046620715d7c852c7b
Zunayeed <zunayeed.edu@gmail.com>
2019-03-16 04:31:05 -0400

    Create ConditionallyLogical.java

---
+++ src/package_3/ConditionallyLogical.java
@@ -0,0 +1,13 @@
+package package_3;
+
+public class ConditionallyLogical {
+public static void main(String... data) {
+	System.out.println(data.length);
+	System.out.println(data[0]);
+if(data.length>=1
+&& (data[0].equals("sound") || data[0].equals ("logic"))
+&& data.length<2) {
+System.out.print(data[0]);
+}
+}
+}
