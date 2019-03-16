1e9f605bc37be07049b6e49f293a84339406ae79
Zunayeed <zunayeed.edu@gmail.com>
2019-03-16 19:24:09 -0400

    Update Pline.java

--- src/package_3/Pline.java
+++ src/package_3/Pline.java
@@ -1,5 +1,10 @@
 package package_3;
 
 public class Pline {
-
+	public void printStormName(String... names) {
+		System.out.println(Arrays.toString(names));
+		}
+		public void printStormNames(String[] names) {
+		System.out.println(Arrays.toString(names));
+		}
 }
