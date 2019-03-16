74f39994f5cef085d3121b43ef1da4c5f5a53846
Zunayeed <zunayeed.edu@gmail.com>
2019-03-16 13:53:43 -0400

    Update OutsideLogic.java

--- src/package_3/OutsideLogic.java
+++ src/package_3/OutsideLogic.java
@@ -3,10 +3,19 @@ package package_3;
 public class OutsideLogic {
 	
 	public static void main(String... weather) {
-		
-		System.out.print(weather[0]!=null
+		/* System.out.print(weather[0]!=null
 		&& weather[0].equals("sunny")
 		&& !false
 		? "Go Outside" : "Stay Inside");
+		*/
+		public void printStormName(String... names) {
+			System.out.println(Arrays.toString(names));
+			}
+			public void printStormNames(String[] names) {
+			System.out.println(Arrays.toString(names));
+			}
+				
+		
+		
 		}
 }
