152a03332efa5cdc747e5dd10f68f55217db712b
Zunayeed <zunayeed.edu@gmail.com>
2019-03-16 09:13:09 -0400

    Create Kansas.java

---
+++ src/tornado/Kansas.java
@@ -0,0 +1,18 @@
+package tornado;
+
+public class Kansas {
+	public static void main(String[] args) {
+		int colorOfRainbow = 10;
+		 final int   red = 5;
+		switch(colorOfRainbow) {
+		
+		case red:
+		System.out.print("Away");
+		break;
+		default:
+			System.out.print("Home");
+			break;
+		}
+		}
+
+}
