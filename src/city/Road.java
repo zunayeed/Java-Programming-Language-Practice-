b06d9b7d18c745979191b207fe4cc2786c7fbe7f
Zunayeed <zunayeed.edu@gmail.com>
2019-03-16 04:26:27 -0400

    Update Road.java

--- src/city/Road.java
+++ src/city/Road.java
@@ -2,7 +2,7 @@ package city;
 
 public class Road {
 	public static void main(String... incheck) {
-		/*
+	/*
 		int intersections = 100;
 		int streets = 200;
 		if (intersections < 150) {
@@ -13,7 +13,7 @@ public class Road {
 		System.out.print("3");
 		else
 		System.out.print("4");
-		*/
+		
 		int x = 10, y = 5;
 		boolean w = true, z = false;
 		System.out.println(x);
@@ -22,6 +22,30 @@ public class Road {
 		w = !z;
 		System.out.println((x+y)+" "+(w ? 5 : 10));
 		System.out.println(incheck.length);
-				
+	*/	
+		int x, y, z;
+
+		  x = 10;
+		  y = 20;
+		  z = 30;
+
+		  // T T
+		  // T F
+		  // F T
+		  // F F
+
+		  //SET A
+		  boolean a = (x < z) && (x == x);
+		  boolean b = (x < z) && (x == z);
+		  boolean c = (x == z) && (x < z);
+		  boolean d = (x == z) && (x > z);
+		  //SET B    
+		  boolean aa = (x < z) & (x == x);
+		  boolean bb = (x < z) & (x == z);
+		  boolean cc = (x == z) & (x < z);
+		  boolean dd = (x == z) & (x > z);
+		System.out.println(d);
+		System.out.println(dd );
+		
 		}
 }
