c3efd7f36e0655bdabf62624712f5ab6fe1af52f
Zunayeed <zunayeed.edu@gmail.com>
2019-03-16 12:30:07 -0400

    Update OutsideLogic.java

--- src/package_3/OutsideLogic.java
+++ src/package_3/OutsideLogic.java
@@ -1,7 +1,9 @@
 package package_3;
 
 public class OutsideLogic {
+	
 	public static void main(String... weather) {
+		
 		System.out.print(weather[0]!=null
 		&& weather[0].equals("sunny")
 		&& !false
