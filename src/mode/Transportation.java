fa3daaf6a1bf4649a78bbf8515e6fb496314cc09
Zunayeed <zunayeed.edu@gmail.com>
2019-03-16 06:29:44 -0400

    Update Transportation.java

--- src/mode/Transportation.java
+++ src/mode/Transportation.java
@@ -2,9 +2,10 @@ package mode;
 
 public class Transportation {
 	public static String travel(int distance) {
-		return distance<1000 ? "train" : 12;
+		return distance<1000 ? "dis" : "cis";
 		}
 		public static void main(String[] answer) {
-		System.out.print(Transportation.travel(500));
+		System.out.println(Transportation.travel(500));
+		System.out.println(answer.length);
 		}
 }
