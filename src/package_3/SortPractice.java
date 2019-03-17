86ffab96c4703d8adc065e81ca7efc1e8895756a
Zunayeed <zunayeed.edu@gmail.com>
2019-03-17 14:12:27 -0400

    Update SortPractice.java

--- src/package_3/SortPractice.java
+++ src/package_3/SortPractice.java
@@ -17,6 +17,14 @@ public class SortPractice {
 		String tiger [] = new String[1];
 		String bear [] = new String[] {};
 		String ohMy [] = new String[0] ;
+		
+		String[][] matrix = new String[1][2];
+		matrix[0][0] = "Don't think you are, know you are."; // m1
+		matrix[0][1] = "I'm trying to free your mind Neo"; // m2
+		// matrix[1][0] = "Is all around you "; // m3
+		matrix[1][1] = "Why oh why didn't I take the BLUE pill?"; // m4
+		
+		
 	}
 
 }
