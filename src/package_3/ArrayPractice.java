d9b559e8b4f9616eb80376985bbbf2356af9cf66
Zunayeed <zunayeed.edu@gmail.com>
2019-03-17 13:49:04 -0400

    Update ArrayPractice.java

--- src/package_3/ArrayPractice.java
+++ src/package_3/ArrayPractice.java
@@ -3,6 +3,8 @@ package package_3;
 public class ArrayPractice {
 static int[] aaa = {2,4,5,6};
 public static void main(String[] args) {
+	int[][] blue = new int[2][2];
+	
 	System.out.println(aaa.length);
 }
 }
