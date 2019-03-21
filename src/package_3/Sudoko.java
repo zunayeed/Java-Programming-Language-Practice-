fc021f438aacd2c8da3b087384dc8689b1fff783
Zunayeed <zunayeed.edu@gmail.com>
2019-03-20 21:56:51 -0400

    Update Sudoko.java

--- src/package_3/Sudoko.java
+++ src/package_3/Sudoko.java
@@ -1,12 +1,12 @@
 package package_3;
 
-public class Sudoko {
-	static int[][] game = new int[6][6];
-	
-	 public static void main(String[] args) {
-	 game[3][3] = 6;
-	 Object[] obj = game;
-	// obj[3] = "X";
-	  System.out.println(game[3][3]);
-	}
-}
+ public class Sudoko {
+ static int[][] game;
+
+ public static void main(String[] args) {
+ game[3][3] = 6;
+ Object[] obj = game;
+ game[3][3] = "X";
+ System.out.println(game[3][3]);
+ }
+ }
