429abdc68b5ed030638c41919b8f01054b6b64b8
Zunayeed <zunayeed.edu@gmail.com>
2019-03-20 22:08:15 -0400

    Create CountCheck.java

---
+++ src/chicago/CountCheck.java
@@ -0,0 +1,16 @@
+package chicago;
+
+public class CountCheck {
+	public static void main(String[] args) {
+		int count = 0;
+		do {
+			
+			do {
+			count++;
+			} while (count < 2);
+			break;
+			} while (true);
+			System.out.println(count);
+	}
+
+}
