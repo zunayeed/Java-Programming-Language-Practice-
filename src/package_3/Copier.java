d1346187aeacdeeca39fa3580d310bb0e528d5ce
Zunayeed <zunayeed.edu@gmail.com>
2019-03-20 21:56:56 -0400

    Update Copier.java

--- src/package_3/Copier.java
+++ src/package_3/Copier.java
@@ -1,8 +1,27 @@
 package package_3;
-
+import java.util.*;
 public class Copier {
 	public static void main(String... original) {
-		String... copy = original;
-		System.out.println(copy.length + " " + copy[0]);
+		List<String> drinks = Arrays.asList("can", "cup");
+		System.out.println(drinks.size());
+		for (int container = drinks.size() - 1; container >= 0; container--)
+		System.out.print(drinks.get(container) + ",");
+		
+		List<String> bottles = Arrays.asList("glass", "plastic");
+		for (int type = 0; type < bottles.size();) {
+		System.out.print(bottles.get(type) + ",");
+		break;
 		}
-}
+		System.out.println("end");
+		
+		String letters = "";
+		while (letters.length() != 2)
+		letters+="a";
+		System.out.println(letters);
+		
+	}
+		
+		
+		}
+	
+	
