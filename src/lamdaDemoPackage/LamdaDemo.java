68b0d38c625214822c049429009e9e28f177cc05
Zunayeed <zunayeed.edu@gmail.com>
2019-03-24 10:06:10 -0400

    Update LamdaDemo.java

--- src/lamdaDemoPackage/LamdaDemo.java
+++ src/lamdaDemoPackage/LamdaDemo.java
@@ -9,7 +9,16 @@ public class LamdaDemo {
 	    // Call getValue(), which is overridden by the previously assigned 
 	    // lambda expression. 
 	    System.out.println("A fixed value: " + myNum.getValue()); 
+	    // Here, a more complex expression is used. 
+	    myNum = () -> Math.random() * 100; 
 	 
+	    // These call the lambda expression in the previous line. 
+	    System.out.println("A random value: " + myNum.getValue()); 
+	    System.out.println("Another random value: " + myNum.getValue()); 
+	 
+	    // A lambda expression must be compatible with the method 
+	    // defined by the functional interface. Therefore, this won't work: 
+	//  myNum = () -> "123.03"; // Error!
 	}
 
 }
