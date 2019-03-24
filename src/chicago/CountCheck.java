ab7fee2ec0442b6c2ee2e0dede8a7d4b3eca915d
Zunayeed <zunayeed.edu@gmail.com>
2019-03-24 08:44:38 -0400

    Update CountCheck.java

--- src/chicago/CountCheck.java
+++ src/chicago/CountCheck.java
@@ -1,5 +1,6 @@
 package chicago;
 
+import java.io.File;
 import java.util.*;
 
 public class CountCheck {
@@ -42,13 +43,70 @@ public class CountCheck {
 				for (Character x : chars) count -=1;
 			}while (count > 0);
 			System.out.println(chars.size());
-			*/
+			
 		int k = 0;
 		for (int i = 10; i > 0; i--) {
 		while (i > 3) i -= 3;
 		k += 1;
 		}
 		System.out.println(k);
+		// File[] hiddenFiles = new File(".").listFiles(File::isHidden);
+		List<String> bottles = Arrays.asList("glass", "plastic");
+		for (int type = 0; type < bottles.size();)
+		System.out.print(bottles.get(type) + ",");
+		break;
+		System.out.print("end");
+		
+		String tie = null;
+		while (tie == null)
+		tie = "shoelace";
+		System.out.print(tie);
+		
+		String alpha = "";
+		for(char current = 'a'; current <= 'z'; current++)
+		alpha += current;
+		System.out.println(alpha);
+		
+		StringBuilder builder = new StringBuilder();
+		String str = new String("Leaves growing");
+		do {
+		System.out.println(str);
+		} while (builder);
+		System.out.println(builder);
+		
+	 int count = 0;
+	 do {
+	 do {
+	 count++;
+	 } while (count < 2);
+	 break;
+	 } while (true);
+	 System.out.println(count);
+		
+	
+	 String tie = null;
+	 while (tie == null);
+	 tie = "shoelace";
+	 System.out.print(tie);
+	 
+	  
+		
+	 StringBuilder sb = new StringBuilder("radical");
+			 sb.insert(sb.length(), "robots");
+			 System.out.println(sb);
+	 
+			 List<String> museums = new ArrayList<>(1);
+			 museums.add("Natural History");
+			 museums.add("Science");
+			 museums.add("Art");
+			 museums.remove(2);
+			 System.out.println(museums);
+		*/	 
+			  StringBuilder b = new StringBuilder("12");
+			  b = b.append("3");
+			   b.reverse();
+			  System.out.println(b);
+			 
 			}
 	}
 			
