bb9a1677b2f0312cf0094b0fd03796211be0d31f
Zunayeed <zunayeed.edu@gmail.com>
2019-03-30 10:28:54 -0400

    Create CheckRename.java

---
+++ src/ai/CheckRename.java
@@ -0,0 +1,10 @@
+package ai;
+
+public class CheckRename {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello Zunayeed  hello zunayeed");
+	}
+
+}
