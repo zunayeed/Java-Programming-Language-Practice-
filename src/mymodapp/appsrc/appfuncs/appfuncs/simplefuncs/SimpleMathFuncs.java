bcd1ad4ed9227a8fd3d21d607c6163189f5d35e3
Zunayeed <zunayeed.edu@gmail.com>
2019-03-30 06:16:16 -0400

    Update SimpleMathFuncs.java

--- src/mymodapp/appsrc/appfuncs/appfuncs/simplefuncs/SimpleMathFuncs.java
+++ src/mymodapp/appsrc/appfuncs/appfuncs/simplefuncs/SimpleMathFuncs.java
@@ -1,12 +1,9 @@
-package appfuncs.simplefuncs; 
+// Some simple math functions, with isFactor() removed. 
+ 
+package appfuncs.simplefuncs;
+import appsupport.supportfuncs.SupportFuncs; 
  
 public class SimpleMathFuncs { 
- 
-  // Determine if a is a factor of b. 
-  public static boolean isFactor(int a, int b) { 
-    if((b%a) == 0) return true; 
-    return false; 
-  } 
  
   // Return the smallest positive factor that a and b have in common. 
   public static int lcf(int a, int b) { 
@@ -17,13 +14,14 @@ public class SimpleMathFuncs {
     int min = a < b ? a : b; 
  
     for(int i = 2; i <= min/2; i++) { 
-      if(isFactor(i, a) && isFactor(i, b))  
+      if(SupportFuncs.isFactor(i, a) && SupportFuncs.isFactor(i, b))  
         return i; 
     } 
  
     return 1;     
   } 
-    // Return the largest positive factor that a and b have in common. 
+ 
+  // Return the largest positive factor that a and b have in common. 
   public static int gcf(int a, int b) { 
     // Factor using positive values. 
     a = Math.abs(a); 
@@ -32,7 +30,7 @@ public class SimpleMathFuncs {
     int min = a < b ? a : b; 
  
     for(int i = min/2; i >= 2; i--) { 
-      if(isFactor(i, a) && isFactor(i, b))  
+      if(SupportFuncs.isFactor(i, a) && SupportFuncs.isFactor(i, b))  
         return i; 
     } 
  
