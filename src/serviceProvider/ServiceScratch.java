a416cd91f96085a537ee583820cfe91cd1f6f586
Zunayeed <zunayeed.edu@gmail.com>
2019-03-31 13:55:39 -0400

    Create ServiceScratch.java

---
+++ src/serviceProvider/ServiceScratch.java
@@ -0,0 +1,12 @@
+package serviceProvider;
+
+import java.util.*;
+
+public class ServiceScratch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static <S> ServiceLoader<S> load(Class <S> serviceType);
+	}
+
+}
